beeac6ae6c0d5630f4dc43b85235ec50dcbfdd9f
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2021-05-08 15:53:33 +0200

    refactor(gmsh): build gmsh in another place

--- bin/build-application-solvcon-devenv.sh
+++ bin/build-application-solvcon-devenv.sh
@@ -3,9 +3,4 @@ DEVENV_TOOL=${HOME}/solvcon/devenv
 DEVENV_FLAVOR="solvcon-devenv"
 SCDE_SRC=${DEVENV_TOOL}/applications/solvcon
 
-source ${DEVENV_TOOL}/scripts/init
-devenv add ${DEVENV_FLAVOR}
-devenv use ${DEVENV_FLAVOR}
-devenv build gmsh
-
 source ${SCDE_SRC}/prepare-solvcon-dev.sh
